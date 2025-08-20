0d64229406f6977eb4f90bc821580ee1ef2d8f71
Jai Sanghvi <85071341+jai-sanghvi@users.noreply.github.com>
2025-08-20 11:22:42 +0000

    Refactor code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://friendly-blancmange-7e94a6.netlify.app/",
+  integrations: [preact()],
+});
